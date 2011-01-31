04271db0c6d2a97f0d9211e127ad363574d3cf36
Nigel Rausch <nigelr@brisbanerails.com>
2011-02-01 08:58:43 +1000

    213727-9390089: reset/create board

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , memoryStore = express.session.MemoryStore;
+        , memoryStore = express.session.MemoryStore;
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
-app.configure(function(){
+app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyDecoder());
@@ -22,22 +21,55 @@ app.configure(function(){
 
 });
 
-app.configure('development', function(){
+app.configure('development', function() {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('production', function(){
+app.configure('production', function() {
   app.use(express.errorHandler());
 });
 
+// board generation
+
+var board = {
+  pieces: {}
+
+  , create: function () {
+    this.buildBackRow("1", "W");
+    this.buildPawnRow("2", "W");
+    for (var col = 3; col <= 6; col ++) {
+      this.buildBlankRow(col)
+    }
+    this.buildPawnRow("7", "B");
+    this.buildBackRow("8", "B");
+    return board.pieces;
+  }
+
+  , buildBackRow: function (row, color) {
+    for (var col = 0; col < 8; col ++) {
+      board.pieces[board.col_labels[col] + row] = color + board.row_pieces[col];
+    }
+  }
+
+  , buildPawnRow: function (row, color) {
+    for (var col = 0; col < 8; col ++) {
+      board.pieces[board.col_labels[col] + row] = color + "P";
+    }
+  }
+
+  , buildBlankRow: function (row) {
+    for (var col = 0; col < 8; col ++) {
+      board.pieces[board.col_labels[col] + row] = "";
+    }
+  }
+  , col_labels: ["a", "b", "c", "d", "e", "f", "g", "h"]
+  , row_pieces: ["R", "N", "B", "Q", "K", "B", "N", "R"]
+}
+
 // Routes
 
-app.get('/', function(req, res){
-  res.render('index', {
-    locals: {
-      title: 'Express'
-    }
-  });
+app.get('/reset', function(req, res) {
+  res.send(board.create());
 });
 
 // Only listen on $ node app.js
--- test/app.test.js
+++ test/app.test.js
@@ -1,4 +1,3 @@
-
 // Run $ expresso
 
 /**
@@ -6,16 +5,26 @@
  */
 
 var app = require('../app')
-  , assert = require('assert');
+        , assert = require('assert');
 
 
 module.exports = {
-  'GET /': function(){
+  'GET /reset - should create board and contents': function() {
     assert.response(app,
-      { url: '/' },
-      { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' }},
-      function(res){
-        assert.includes(res.body, '<title>Express</title>');
-      });
+    { url: '/reset' },
+    { status: 200, headers: { 'Content-Type': 'application/json' }}
+            , function(res) {
+              assert.eql(JSON.parse(res.body), {
+                a1: 'WR', b1: 'WN', c1: 'WB', d1: 'WQ', e1: 'WK', f1: 'WB', g1: 'WN', h1: 'WR'
+                , a2: 'WP', b2: 'WP', c2: 'WP', d2: 'WP', e2: 'WP', f2: 'WP', g2: 'WP', h2: 'WP'
+                , a3: '', b3: '', c3: '', d3: '', e3: '', f3: '', g3: '', h3: ''
+                , a4: '', b4: '', c4: '', d4: '', e4: '', f4: '', g4: '', h4: ''
+                , a5: '', b5: '', c5: '', d5: '', e5: '', f5: '', g5: '', h5: ''
+                , a6: '', b6: '', c6: '', d6: '', e6: '', f6: '', g6: '', h6: ''
+                , a7: 'BP', b7: 'BP', c7: 'BP', d7: 'BP', e7: 'BP', f7: 'BP', g7: 'BP', h7: 'BP'
+                , a8: 'BR', b8: 'BN', c8: 'BB', d8: 'BQ', e8: 'BK', f8: 'BB', g8: 'BN', h8: 'BR'
+              }
+                      )
+            });
   }
 };
